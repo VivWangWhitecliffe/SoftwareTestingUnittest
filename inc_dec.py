4cfee3129508765dc9e93c202aec3961a3c14bbf
Vivian Wang <VivianW@whitecliffe.ac.nz>
2024-05-22 09:47:27 +1200

    lab

--- inc_dec.py
+++ inc_dec.py
@@ -1,18 +1,11 @@
 def increment(x): 
-
-    return x + 1 
- 
+    return (x+1)
 
 def decrement(x): 
-
-    return x - 1 
+    return (x-1)
 
 def tothepoweroftwo(x):
     return x **2
 
-a=increment(3)
-b=decrement(5)
-c=tothepoweroftwo(4)
 
-print(a,b,c)
 
--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import unittest   # The test framework
 class Test_TestaddFuction(unittest.TestCase):
     def testadd(self):
         self.assertEqual(app.add(3,1), 4)  # Test add function 
-    def testadd(self):
+    def testaddinvalid(self):
         self.assertEqual(app.add(3,2), 5)  # Test add function 
 
 
